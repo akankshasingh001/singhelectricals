81a402c95952240c11909201bbe53116bf66248c
Akanksha Singh <singhakanksha001@gmail.com>
2020-05-18 17:18:05 -0400

    App.js changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './Layouts/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import '../index.css';
 class App extends Component {
   render() {
     return (
-      <div style={{ width: '100%' }}>
+      <Router>
         <Header />
-      </div>
+      </Router>
     );
   }
 }
